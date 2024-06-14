ddeec8a5980fda38af9f75b12b53ee05853d1612
Eric Hechavarria <erichech@gmail.com>
2024-06-14 19:59:48 -0400

    fix useIsOwner

--- src/services/hooks/useIsOwner.ts
+++ src/services/hooks/useIsOwner.ts
@@ -11,13 +11,13 @@ export const useIsOwner = ({
   dataKey: Maybe<Nullable<string>>;
   uid: Maybe<Nullable<string>>;
 }) => {
-  const [isOwner, setIsOwner] = useState<Nullable<boolean>>();
+  const [isOwner, setIsOwner] = useState<Nullable<number | false>>();
 
   useEffect(() => {
     if (dataKey && uid) {
       try {
         getOwner({ dataType, dataKey, uid })
-          .then((ownerTimestamp) => setIsOwner(typeof ownerTimestamp === "number"))
+          .then(setIsOwner)
           .catch(() => setIsOwner(false)); // catch the firebase error that arises if you are not the owner
       } catch (e) {
         // Really, really throw the error away (why trashing it seem to work?)
